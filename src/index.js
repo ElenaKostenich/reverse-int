2a92749dc2688cc86a8a809a02c672ea939a0218
Elena <j45k56@gmail.com>
2022-01-07 03:23:49 +0300

    add: solution task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    let positiveNum;
+    if (n < 0) {
+      positiveNum = Math.abs(n);
+    } else {
+        positiveNum = n
+    }
+    let nStr = positiveNum.toString();
+    let rev = '';
+    
+    for (let i = 0; i < nStr.length; i++) {
+      rev = nStr[i] + rev;
+    }
+    return Number(rev);
 }
